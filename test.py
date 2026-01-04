f697020af90944a11b1fc7bfa8a9b0a93ea25af7
muhammad571634 <84302978+muhammad571634@users.noreply.github.com>
2026-01-05 01:52:38 +0500

    Update test.py

--- test.py
+++ test.py
@@ -49,7 +49,6 @@ save_queue()
 
 (f"{len(queue)+1}. {name} — {phone}")
 
-await update.message.reply_text("ok")
         f"✅ Navbatga qo‘shildingiz!\n"
         f"👤 Ism: {name}\n"
         f"📞 Telefon: {phone}\n"
